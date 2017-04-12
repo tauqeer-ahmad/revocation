73f11f10f67a8a8df612e88c6a953af7fae91368
Tauqeer Ahmad <tauqeer.ahmad2008@gmail.com>
2017-04-13 02:08:23 +0500

    minor section updates

--- app/assets/javascripts/style.js
+++ app/assets/javascripts/style.js
@@ -5,7 +5,7 @@ $(document).on('turbolinks:load', function() {
 
   $( "#tabs" ).tabs({
     beforeLoad: function(event, ui) {
-      ui.panel.html('<div class="tab-loder"><img src="/assets/loading.gif" width="24" height="24" style="vertical-align:middle;"> Loading...</div>');
+      ui.panel.html('<div class="tab-loader"><img src="/assets/loading.gif" width="24" height="24" style="vertical-align:middle;"> Loading...</div>');
     }
   }).addClass( "ui-tabs-vertical ui-helper-clearfix" );
   $( "#tabs li" ).removeClass( "ui-corner-top" ).addClass( "ui-corner-left" );
