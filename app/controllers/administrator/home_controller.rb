02bcb647f470e51580635a42c1091e0ba91e971b
Ahsan Ellahi <ahsan.ellahi@square63.com>
2017-05-15 05:03:43 +0500

    Application: Added pin board feature and implemented for adminisitrators

--- app/controllers/administrator/home_controller.rb
+++ app/controllers/administrator/home_controller.rb
@@ -1,4 +1,21 @@
 class Administrator::HomeController < ApplicationController
   def index
+    @rolling = {
+      students: Student.count,
+      teachers: Teacher.count,
+      klasses: Klass.count,
+      sections: Section.count,
+    }
+  end
+
+  def pin_board
+  end
+
+  def create_note
+    @note = current_administrator.notes.create(heading: params[:heading], description: params[:description])
+  end
+
+  def delete_note
+    current_administrator.notes.find(params[:note_id]).delete
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170508160833) do
+ActiveRecord::Schema.define(version: 20170514223831) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,15 @@ ActiveRecord::Schema.define(version: 20170508160833) do
     t.string   "code"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "notes", force: :cascade do |t|
+    t.string   "heading",     limit: 100, null: false
+    t.text     "description"
+    t.integer  "user_id"
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
+    t.index ["user_id"], name: "index_notes_on_user_id", using: :btree
   end
 
   create_table "section_students", force: :cascade do |t|
@@ -123,5 +132,6 @@ ActiveRecord::Schema.define(version: 20170508160833) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "notes", "users"
   add_foreign_key "users", "institutions"
 end
