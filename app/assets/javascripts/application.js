42abedc92a8c01e0cd5ec2cf0f93b8d45b7ab3ba
Ahsan Ellahi <ahsan.ellahi@square63.com>
2017-05-17 18:14:30 +0500

    Toastr: Fixed config again

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -23,5 +23,27 @@
 //= require moment
 //= require bootstrap-datetimepicker
 //= require select2-full
-//= require toastr_config
 //= require_tree .
+
+toastr.options = {
+  'closeButton': true,
+  'debug': false,
+  'newestOnTop': true,
+  'progressBar': true,
+  'preventDuplicates': false,
+  'onclick': null,
+  'showDuration': '300',
+  'hideDuration': '1000',
+  'timeOut': '5000',
+  'extendedTimeOut': '1000',
+  'showEasing': 'swing',
+  'hideEasing': 'linear',
+  'showMethod': 'fadeIn',
+  'hideMethod': 'fadeOut'
+}
+
+if ($(window).width() < 990) {
+  toastr.options.positionClass = 'toast-top-center'
+} else {
+  toastr.options.positionClass = 'toast-top-right'
+}
