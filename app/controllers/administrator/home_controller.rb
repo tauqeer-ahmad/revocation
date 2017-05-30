b26026b09e8411619c972be820adfc8fddc1af65
Ahsan Ellahi <ahsan.ellahi@square63.com>
2017-05-31 00:52:21 +0500

    Application: Added account locking functionality

--- app/controllers/administrator/home_controller.rb
+++ app/controllers/administrator/home_controller.rb
@@ -1,4 +1,6 @@
 class Administrator::HomeController < ApplicationController
+  layout :resolve_layout
+
   def index
     @rolling = {
       students: Student.count,
@@ -7,4 +9,28 @@ class Administrator::HomeController < ApplicationController
       sections: Section.count,
     }
   end
+
+  def lock_account
+    session[:lock_account] = true
+  end
+
+  def unlock_account
+    if current_user.valid_password?(params[:password])
+      flash[:notice] = 'Successfully unlocked your account!'
+    else
+      flash[:alert] = 'You entered incorrect password!'
+    end
+
+    redirect_to root_path
+  end
+
+  private
+    def resolve_layout
+      case action_name
+        when 'lock_account'
+          'empty'
+        else
+          'application'
+      end
+    end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,11 @@
 class SessionsController < Devise::SessionsController
+  layout 'empty'
+
+  def destroy
+    signed_out = (Devise.sign_out_all_scopes ? sign_out : sign_out(resource_name))
+    set_flash_message! :notice, :signed_out if signed_out
+    yield if block_given?
+    session[:lock_account] = false
+    respond_to_on_destroy
+  end
 end
