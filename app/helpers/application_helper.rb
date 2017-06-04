f5970a7a32f8b27acd566fae377624e7b2e32451
Ahsan Ellahi <ahsan.ellahi@square63.com>
2017-06-04 19:48:39 +0500

    Exams: Updated UI and styling

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -96,7 +96,7 @@ module ApplicationHelper
 
   def calculate_percentage(value, total)
     return 0.0 if total.zero?
-    ((value.to_f/total.to_f)*100).round(1)
+    ((value.to_f / total) * 100).round(1)
   end
 
   def calculate_average(values, count)
