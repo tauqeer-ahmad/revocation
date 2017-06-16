e47ccc9b9c9161a8f283c5919dc5b38df092afd5
Ahsan Ellahi <ahsan.ellahi@square63.com>
2017-06-16 13:35:08 +0500

    Production: Added smtp settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,4 +83,16 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.default_url_options = { host: 'revoech.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'gmail.com',
+    user_name:            'revoech@gmail.com',
+    password:             'FL7r460hmJ2k4qcJO2ybpy7e',
+    authentication:       'plain',
+    enable_starttls_auto: true
+  }
 end
