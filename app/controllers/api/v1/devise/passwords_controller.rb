71fa266b0aa978a4ebf8606b26bfb4fb262b2024
Tauqeer Ahmad <tauqeer.ahmad2008@gmail.com>
2017-07-09 04:15:35 +0500

    Devise: Added api to reset password from mobile apps

---
+++ app/controllers/api/v1/devise/passwords_controller.rb
@@ -0,0 +1,34 @@
+class Api::V1::Devise::PasswordsController < Api::V1::BaseController
+  skip_before_action :require_login, only: [:create, :update]
+
+  # POST /resource/password
+  def create
+    resource_class = User.get_resource_and_key_name(request.fullpath)
+    resource = resource_class.send_reset_password_instructions(resource_params)
+
+    if resource.errors.empty?
+      render json: {message: "You will receive an email to reset your password."}
+    else
+      render json: resource.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PUT /resource/password
+  def update
+    resource_class = User.get_resource_and_key_name(request.fullpath)
+    resource = resource_class.reset_password_by_token(resource_params)
+
+    if resource.errors.empty?
+      resource.regenerate_access_token
+      render json: resource, serializer: LoginSuccessSerializer, subdomain: request.subdomain
+    else
+      #set_minimum_password_length
+      render json: resource.errors, status: :unprocessable_entity
+    end
+  end
+
+  protected
+    def resource_params
+      params.fetch(:user)
+    end
+end
