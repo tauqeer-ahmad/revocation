6c0d1c9b4d4d3d52e0fd721c3dcd25cb20cf85ed
Ahsan Ellahi <ahsan.ellahi@square63.com>
2017-07-31 00:10:51 +0500

    Testimonial: Added model level validations

--- app/models/testimonial.rb
+++ app/models/testimonial.rb
@@ -3,6 +3,8 @@ class Testimonial < ApplicationRecord
 
   scope :active, -> { where(status: 'active') }
 
+  validates :heading, :message, presence: true
+
   def active?
     self.status == 'active'
   end
