7a09740d593bce558d56637a4da574c582d7ca4a
Tauqeer Ahmad <tauqeer.ahmad2008@gmail.com>
2017-08-19 01:31:27 +0500

    Added student base controller

--- app/controllers/api/v1/student/results_controller.rb
+++ app/controllers/api/v1/student/results_controller.rb
@@ -1,4 +1,4 @@
-class Api::V1::Student::ResultsController < Api::V1::BaseController
+class Api::V1::Student::ResultsController < Api::V1::Student::StudentBaseController
   def index
     @exams = current_term.exams.eager_load(exam_marks: [:subject, :section, :klass]).where("exam_marks.student_id =?", current_user.id)
     render json: @exams
