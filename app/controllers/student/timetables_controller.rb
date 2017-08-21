5988056e02d23fa1456a6164ab3b5b4b7ac62b5d
Tauqeer Ahmad <tauqeer.ahmad2008@gmail.com>
2017-08-21 22:39:04 +0500

    Incorporated feedbacks

--- app/controllers/student/timetables_controller.rb
+++ app/controllers/student/timetables_controller.rb
@@ -1,7 +1,7 @@
 class Student::TimetablesController < ApplicationController
   def index
     @section = current_student.current_section(current_term.id)
-    @timetables = @section.timetables.by_bay_of_week
+    @timetables = @section.timetables.by_day_of_week
     @days_hash = Timetable::DAYS
   end
 end
