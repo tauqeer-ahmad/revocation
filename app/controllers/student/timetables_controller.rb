ab3ff07687783b20548e9dc4c9bb3af54206ce12
Tauqeer Ahmad <tauqeer.ahmad2008@gmail.com>
2017-08-22 18:44:36 +0500

    Direct use of variable to display days

--- app/controllers/student/timetables_controller.rb
+++ app/controllers/student/timetables_controller.rb
@@ -2,6 +2,5 @@ class Student::TimetablesController < ApplicationController
   def index
     @section = current_student.current_section(current_term.id)
     @timetables = @section.timetables.by_day_of_week
-    @days_hash = Timetable::DAYS
   end
 end
