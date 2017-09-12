7145233a08310c9b9830080595e6ea532ded75c9
taimur-akhtar <taimur.akhtar@square63.com>
2017-09-12 09:31:05 +0500

    Student: Removed catching mechanism.

--- app/serializers/child_serializer.rb
+++ app/serializers/child_serializer.rb
@@ -1,16 +1,12 @@
 class ChildSerializer < ActiveModel::Serializer
   attributes :id, :name, :email, :klass_name, :section_name, :avatar_url, :thumb_avatar_url, :medium_avatar_url
 
-  def current_term
-    @current_term ||= Term.active_term
-  end
-
   def klass_name
-    object.current_section(current_term.id).klass.name
+    object.current_section(Term.active_term).klass.name
   end
 
   def section_name
-    object.current_section(current_term.id).name
+    object.current_section(Term.active_term).name
   end
 
 
