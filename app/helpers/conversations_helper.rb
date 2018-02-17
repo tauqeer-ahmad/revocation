954c02b0ad04093aa8b7697beddabbc3d6ef17e0
Ahsan Ellahi <ahsan.ellahi896@gmail.com>
2018-02-17 16:17:25 +0500

    Conversaton: Fixed messages count query to display at header for notifications

--- app/helpers/conversations_helper.rb
+++ app/helpers/conversations_helper.rb
@@ -1,2 +1,7 @@
 module ConversationsHelper
+  def unread_messages_count()
+    Conversation.user_conversation(current_user.id, current_term.id).map do |conversation|
+      conversation.messages.unread.where.not(user_id: current_user.id)
+    end.flatten.count
+  end
 end
