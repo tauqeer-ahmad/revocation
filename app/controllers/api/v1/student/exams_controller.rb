3140db64312388a306925f4be65c1cc734483971
Tauqeer Ahmad <tauqeer.ahmad2008@gmail.com>
2018-02-22 15:18:04 +0500

    Updated results api for student

--- app/controllers/api/v1/student/exams_controller.rb
+++ app/controllers/api/v1/student/exams_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::Student::ExamsController < Api::V1::Student::StudentBaseController
-  before_action :set_exam, only: [:show, :results]
+  before_action :set_exam, only: [:show]
   def index
     @exams = current_term.exams.includes(exam_timetables: [:subject, :klass])
     render json: @exams, student_id: current_user.id
