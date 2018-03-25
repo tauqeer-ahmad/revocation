77522e1ed005523b017e8c8e4b7395d477b48cc6
Ahsan Ellahi <ahsan.ellahi896@gmail.com>
2018-03-25 23:33:40 +0500

    Student::Attendances: Updated structure according to new StudentAttendance one

--- app/controllers/student/attendance_controller.rb
+++ app/controllers/student/attendance_controller.rb
@@ -1,6 +1,9 @@
 class Student::AttendanceController < ApplicationController
   def report
-    @sheets_by_month = Attendance.of_student_and_term(selected_user.id, current_term.id)
-                                 .group_by{ |a| Date::MONTHNAMES[a.attendance_sheet.name.month] }
+    params[:student_id] = current_user.id
+
+    @formated_results, @key_to_dates, @month_statistics, @month_late_statistics, @attendances, @start_range, @end_range, @section = StudentAttendance.fetch_report_data(params, current_term)
+
+    @section = current_user.current_section(current_term.id)
   end
 end
