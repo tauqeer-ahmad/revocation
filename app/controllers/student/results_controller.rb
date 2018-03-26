bf3422d34520ebffea18123b749be30ec08285e8
Ahsan Ellahi <ahsan.ellahi896@gmail.com>
2018-03-26 23:58:37 +0500

    Student::ResultsController: Added student detailed information for index

--- app/controllers/student/results_controller.rb
+++ app/controllers/student/results_controller.rb
@@ -1,6 +1,7 @@
 class Student::ResultsController < ApplicationController
   def index
     @student = current_user
+    @results = @student.results_json(current_term)
     @section = @student.current_section(current_term.id)
     @exam_marks = @student.exam_marks.where(section_id: @section.id, term_id: current_term.id)
     @exam_grouped = @exam_marks.group_by(&:exam_id)
