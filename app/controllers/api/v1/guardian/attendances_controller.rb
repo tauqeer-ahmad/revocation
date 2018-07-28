b6490c895c637a2d560b5cee1f61aa8ed42804e6
Ahsan Ellahi <ahsan.ellahi896@gmail.com>
2018-07-29 02:34:48 +0500

    Api: Removed scope of month and year params logic for attendances

--- app/controllers/api/v1/guardian/attendances_controller.rb
+++ app/controllers/api/v1/guardian/attendances_controller.rb
@@ -1,7 +1,6 @@
 class Api::V1::Guardian::AttendancesController < Api::V1::Guardian::GuardianBaseController
   def index
-    @attendances = StudentAttendance.for_month_and_year(params[:month], params[:year])
-                                    .of_student_and_term(selected_user.id, current_term.id)
+    @attendances = StudentAttendance.of_student_and_term(selected_user.id, current_term.id)
 
     render json: @attendances
   end
